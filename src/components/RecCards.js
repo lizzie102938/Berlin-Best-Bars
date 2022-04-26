6941bf1ff0f06f372d10a73250aeb82fd8c2c01f
lizzie lerwill <lizzielerwill@hotmail.com>
2022-04-26 15:56:59 +0200

    changes before heroku push

--- src/components/RecCards.js
+++ src/components/RecCards.js
@@ -6,13 +6,21 @@ class RecCards extends Component {
     return(
       <div className='RecCards'>
         <div className='RecCards-text'>
-          <h4>Top picks this week..</h4>
+          <h4>Top picks this week...</h4>
         </div>
         <div className='RecCards-photocards'>
-          <h2 className='RecCards-1 RecCards-name'><span>Das Hotel</span></h2>
-          <h2 className='RecCards-2 RecCards-name'><span>Klunkerkranich</span></h2>
-          <h2 className='RecCards-3 RecCards-name'><span>ORA</span></h2>
-          <h2 className='RecCards-4 RecCards-name'><span>Kantine am Berghain</span></h2>
+          <a target='_blank' rel='noopener noreferrer' href='https://dashotel.radio/'>
+            <h2 className='RecCards-1 RecCards-name'><span>Das Hotel</span></h2>
+          </a>
+          <a target='_blank' rel='noopener noreferrer' href='http://klunkerkranich.org/'>
+            <h2 className='RecCards-2 RecCards-name'><span>Klunkerkranich</span></h2>
+          </a>
+          <a target='_blank' rel='noopener noreferrer' href='https://ora.berlin/'>
+            <h2 className='RecCards-3 RecCards-name'><span>ORA</span></h2>
+          </a>
+          <a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/KantineamBerghain'>
+            <h2 className='RecCards-4 RecCards-name'><span>Kantine am Berghain</span></h2>
+          </a>
         </div>
       </div>
     )
